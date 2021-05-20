a0d3a565e336cf115d21661098657086281afabc
montserratortega <al174395@edu.uaa.mx>
2021-05-19 20:10:23 -0500

    Fix app.js

--- resources/views/contenido/contenido.blade.php
+++ resources/views/contenido/contenido.blade.php
@@ -2,11 +2,11 @@
 @section('contenido')
 
  <template v-if="menu==0">
-    <categoria></categoria>
+    <h1> Hello there! </h1>
  </template>
 
  <template v-if="menu==1">
-    <h1>contenido 1</h1>
+    <categoria></categoria>
  </template>
 
  <template v-if="menu==2">
