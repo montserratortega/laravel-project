53a773466bd8c3d29877477552a0eed0b8c6aebe
montserratortega <al174395@edu.uaa.mx>
2021-05-23 17:17:22 -0500

    Controlador Usuario

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,9 @@ Route::post('/cliente/registrar', 'ClienteController@store');
 Route::put('/cliente/actualizar', 'ClienteController@update');
 
 Route::get('/rol', 'RolController@index');
+
+Route::get('/user', 'UserController@index');
+Route::post('/user/registrar', 'UserController@store');
+Route::put('/user/actualizar', 'UserController@update');
+Route::put('/user/desactivar', 'UserController@desactivar');
+Route::put('/user/activar', 'UserController@activar');
